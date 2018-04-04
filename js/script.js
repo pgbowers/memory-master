d50fae7fd6404d9f720b3ba349ec4a93031dc7c5
pgbowers <pgbowers@gmail.com>
2018-04-04 07:54:02 -0300

    Apr 4 - reset works, initialize finction works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let matches = 0;
+let delay = 1200;
+
 // some variables to hold the buttons
 var playGameButton = document.getElementById("playGame-button");
 var gameOverButton = document.getElementById("gameOver-button");
@@ -53,96 +59,99 @@ const cardsArray = [{
   },
 ];
 
-const gameGrid = cardsArray
-    .concat(cardsArray)
-    .sort(() => 0.5 - Math.random());
+initialize();
 
-let firstGuess = '';
-let secondGuess = '';
-let count = 0;
-let matches = 0;
-let delay = 1200;
+function initialize(firstTime) {
+    const gameGrid = cardsArray
+        .concat(cardsArray)
+        .sort(() => 0.5 - Math.random());
 
-const game = document.getElementById('game');
-const grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
 
-gameGrid.forEach(item => {
-    const {
-        name,
-        img
-    } = item;
+    console.log("Length is: ", gameGrid.length);
 
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.name = name;
 
-    const front = document.createElement('div');
-    front.classList.add('front');
 
-    const back = document.createElement('div');
-    back.classList.add('back');
-    back.style.backgroundImage = `url(${img})`;
+    const game = document.getElementById('game');
+    const grid = document.createElement('section');
+    grid.setAttribute('class', 'grid');
+    game.appendChild(grid);
 
-    grid.appendChild(card);
-    card.appendChild(front);
-    card.appendChild(back);
-});
+    gameGrid.forEach(item => {
+        const {
+            name,
+            img
+        } = item;
 
-// function to record a match of two cards
-const match = () => {
-    const selected = document.querySelectorAll('.selected');
-    selected.forEach(card => {
-        card.classList.add('match');
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.name = name;
+
+        const front = document.createElement('div');
+        front.classList.add('front');
+
+        const back = document.createElement('div');
+        back.classList.add('back');
+        back.style.backgroundImage = `url(${img})`;
+
+        grid.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
     });
-};
 
-// function to reset the cards if no match was made
-const resetGuesses = () => {
-    firstGuess = '';
-    secondGuess = '';
-    count = 0;
-    var selected = document.querySelectorAll('.selected');
-    selected.forEach(card => {
-        card.classList.remove('selected');
-    });
-};
+    // function to record a match of two cards
+    const match = () => {
+        const selected = document.querySelectorAll('.selected');
+        selected.forEach(card => {
+            card.classList.add('match');
+        });
+    };
 
-grid.addEventListener('click', event => {
-    const clicked = event.target;
-    if (
-        // if click was not on a card
-        clicked.nodeName === 'SECTION' ||
+    // function to reset the cards if no match was made
+    const resetGuesses = () => {
+        firstGuess = '';
+        secondGuess = '';
+        count = 0;
+        var selected = document.querySelectorAll('.selected');
+        selected.forEach(card => {
+            card.classList.remove('selected');
+        });
+    };
 
-        // if already matched, do not click
-        clicked.parentNode.classList.contains('match')
-    ) {
-        return;
-    }
-    if (count < 2) {
-        count++;
-        if (count === 1) {
-            firstGuess = clicked.parentNode.dataset.name;
-            clicked.parentNode.classList.add('selected');
-        } else {
-            secondGuess = clicked.parentNode.dataset.name;
-            clicked.parentNode.classList.add('selected');
+    grid.addEventListener('click', event => {
+        const clicked = event.target;
+        if (
+            // if click was not on a card
+            clicked.nodeName === 'SECTION' ||
+
+            // if already matched, do not click
+            clicked.parentNode.classList.contains('match')
+        ) {
+            return;
         }
-        if (firstGuess && secondGuess) {
-            if (firstGuess === secondGuess) {
-                matches++;
-                setTimeout(match, delay);
-                console.log(matches);
+        if (count < 2) {
+            count++;
+            if (count === 1) {
+                firstGuess = clicked.parentNode.dataset.name;
+                clicked.parentNode.classList.add('selected');
+            } else {
+                secondGuess = clicked.parentNode.dataset.name;
+                clicked.parentNode.classList.add('selected');
             }
-            setTimeout(resetGuesses, delay);
+            if (firstGuess && secondGuess) {
+                if (firstGuess === secondGuess) {
+                    matches++;
+                    setTimeout(match, delay);
+                    console.log(matches);
+                }
+                setTimeout(resetGuesses, delay);
+            }
         }
-    }
-    if(matches === 2) {
-        setTimeout(gameOver, 2000);
-    }
+        if (matches === gameGrid.length/2) {
+            setTimeout(gameOver, 2000);
+        }
 
-});
+    });
+}
 
 function gameOver() {
     document.getElementById("game").style.display = "none";
@@ -150,9 +159,15 @@ function gameOver() {
 }
 
 function playAgain() {
-    document.getElementById("game").style.display = "flex";
+
     document.getElementById("gameOver-screen").style.display = "none";
     //reset the matches counter
     matches = 0;
+    count = 0;
+    document.getElementById("game").style.display = "flex";
+    //initialize();
+
+    // reloads the game
+    document.location.reload();
 }
 playAgainButton.addEventListener("click", playAgain);
